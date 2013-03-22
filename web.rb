f229c49bad1f06c6943c942c89efc2942f23ceb1
Jeremy Toussaint <jt151077@gmail.com>
2013-03-22 14:09:28 +0100

    bug fix

--- web.rb
+++ web.rb
@@ -6,7 +6,7 @@ require 'fileutils'
 configure do
   Mongoid.configure do |config|
     if ENV['MONGOLAB_URI']
-      uri = URI.parse(ENV['MONGOHQ_URL'])
+      uri = URI.parse(ENV['MONGOLAB_URI'])
       conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
       config.master = conn.db(uri.path.gsub(/^\//, ''))
     else
