7d79a9016b78ad7c9fcf0655a758e92dac9a1aa1
Jeremy Toussaint <jt151077@gmail.com>
2013-03-22 13:45:40 +0100

    load file as default

--- web.rb
+++ web.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+require 'mongoid'
+require 'digest'
+require 'fileutils'
+
+configure do
+  Mongoid.configure do |config|
+    if ENV['MONGOLAB_URI']
+      uri = URI.parse(ENV['MONGOHQ_URL'])
+      conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+      config.master = conn.db(uri.path.gsub(/^\//, ''))
+    else
+      name = "my_local_development_database"
+      host = "localhost"
+      config.master = Mongo::Connection.new.db(name)
+    end
+  end
+end
 
 get '/' do
-  "Hello, jeremy and richard heroku rocks, even more!!!"
+  send_file File.join('public', 'index.html');
 end
